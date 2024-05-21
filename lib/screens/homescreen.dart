f3ab134d9310de8c89f2a5331f5b5f2a9045393b
1psrishti <1psrishti@gmail.com>
2024-05-22 00:02:45 +0530

    initial commit

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text("My Lists"),
+        //TODO: add three dot bars or the plus button ?
+      ),
+      // body:
+    );
+  }
+}
